791a0c025a82549bafa9fb2fe03abe92183d8b44
Namita-Scalent <namita@scalent.io>
2026-08-25 12:33:48 +0530

    remove call from wire of deleted file

--- backend/services/core/app/wire.go
+++ backend/services/core/app/wire.go
@@ -45,30 +45,12 @@ var CoreModuleSet = wire.NewSet(
 	service.NewUserServiceImpl,
 	wire.Bind(new(service.UserService), new(*service.UserServiceImpl)),
 
-	repo.NewEmailListRepoImpl,
-	wire.Bind(new(service.EmailListRepo), new(*repo.EmailListRepoImpl)),
-
-	service.NewEmailListServiceImpl,
-	wire.Bind(new(service.EmailListService), new(*service.EmailListServiceImpl)),
-
-	repo.NewLeadRepoImpl,
-	wire.Bind(new(service.LeadRepo), new(*repo.LeadRepoImpl)),
-
-	service.NewLeadServiceImpl,
-	wire.Bind(new(service.LeadService), new(*service.LeadServiceImpl)),
-
 	//login module
 	repo.NewLoginRepoImpl,
 	wire.Bind(new(service.LoginRepo), new(*repo.LoginRepoImpl)),
 
 	service.NewLoginServiceImpl,
 	wire.Bind(new(service.LoginService), new(*service.LoginServiceImpl)),
-
-	repo.NewUserSettingRepoImpl,
-	wire.Bind(new(service.UserSettingRepo), new(*repo.UserSettingRepoImpl)),
-
-	service.NewUserSettingServiceImpl,
-	wire.Bind(new(service.UserSettingService), new(*service.UserSettingServiceImpl)),
 
 // -----==-----==DO NOT ADD CODE BELOW THIS LINE------
 )
